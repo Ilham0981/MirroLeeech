14362c52d4bbbbceb20839af351356538a31ff05
Ilham0981 <155692730+Ilham0981@users.noreply.github.com>
2024-01-11 11:00:57 +0700

    Update run.py

--- run.py
+++ run.py
@@ -3,8 +3,8 @@ import subprocess
 # Install dependencies
 subprocess.run(['pip3', 'install', '-r', 'requirements.txt'])
 
-# Jalankan update.py
-subprocess.run(['python3', 'update.py'])
+
+
 
 # Jalankan python3 -m bot
 subprocess.run(['python3', '-m', 'bot'])
