9f76001df002af8ef0bf169fe8ea3bd1e906723d
Ilham0981 <155692730+Ilham0981@users.noreply.github.com>
2024-01-11 11:10:37 +0700

    Update run.py

--- run.py
+++ run.py
@@ -1,28 +1,24 @@
 import subprocess
 
-# Update package lists and install dependencies
-subprocess.run(['apt-get', 'update'])
-subprocess.run(['DEBIAN_FRONTEND=noninteractive', 'apt-get', 'install', '-y', 
-                'python3', 'python3-pip', 'aria2', 'qbittorrent-nox', 'tzdata', 'p7zip-full', 'p7zip-rar', 'xz-utils', 'curl', 'pv', 'jq', 'ffmpeg',
-                'locales', 'git', 'unzip', 'rtmpdump', 'libmagic-dev', 'libcurl4-openssl-dev', 'libssl-dev', 'libc-ares-dev', 'libsodium-dev',
-                'libcrypto++-dev', 'libsqlite3-dev', 'libfreeimage-dev', 'libpq-dev', 'libffi-dev'])
+# Update and install dependencies
+subprocess.run(['apt-get', '-y', 'update'])
+subprocess.run(['DEBIAN_FRONTEND=noninteractive', 'apt-get', 'install', '-y', 'python3', 'python3-pip', 'aria2', 'qbittorrent-nox', 'tzdata', 'p7zip-full', 'p7zip-rar', 'xz-utils', 'curl', 'pv', 'jq', 'ffmpeg', 'locales', 'git', 'unzip', 'rtmpdump', 'libmagic-dev', 'libcurl4-openssl-dev', 'libssl-dev', 'libc-ares-dev', 'libsodium-dev', 'libcrypto++-dev', 'libsqlite3-dev', 'libfreeimage-dev', 'libpq-dev', 'libffi-dev'])
 
 # Generate locale
 subprocess.run(['locale-gen', 'en_US.UTF-8'])
 
 # Download and install megasdkrest
 cpu = subprocess.check_output(['uname', '-m']).decode().strip()
-arch = "amd64" if cpu == "x86_64" else "i386" if cpu == "x86" else "arm64" if cpu == "aarch64" else cpu
-subprocess.run(['curl', '-L', f'https://github.com/anasty17/megasdkrest/releases/download/latest/megasdkrest-{arch}',
-                '-o', '/usr/local/bin/megasdkrest'])
+arch = ''
+if cpu == 'x86_64':
+    arch = 'amd64'
+elif cpu == 'x86':
+    arch = 'i386'
+elif cpu == 'aarch64':
+    arch = 'arm64'
+else:
+    arch = cpu
+
+url = f'https://github.com/anasty17/megasdkrest/releases/download/latest/megasdkrest-{arch}'
+subprocess.run(['curl', '-L', url, '-o', '/usr/local/bin/megasdkrest'])
 subprocess.run(['chmod', '+x', '/usr/local/bin/megasdkrest'])
-
-# Set environment variables
-subprocess.run(['export', 'LANG=en_US.UTF-8'])
-subprocess.run(['export', 'LANGUAGE=en_US:en'])
-
-# Install Python dependencies
-subprocess.run(['pip3', 'install', '--no-cache-dir', '-r', '/usr/src/app/requirements.txt'])
-
-# Run the Python script
-subprocess.run(['python3', 'run.py'])
