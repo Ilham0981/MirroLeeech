5b16c1fb6a63f06dec149a77b23d9d048965a935
Ilham0981 <155692730+Ilham0981@users.noreply.github.com>
2024-01-11 10:57:36 +0700

    Update run.py

--- run.py
+++ run.py
@@ -1,4 +1,7 @@
 import subprocess
 
+# Install dependencies
+subprocess.run('pip3 install -r requirements.txt', shell=True)
+
 # Jalankan python3 -m bot
 subprocess.run('python3 -m bot', shell=True)
